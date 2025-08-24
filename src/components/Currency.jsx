1624706e69f2db7d69804b9e207af89e37465e99
fnur0 <fnurpekmez@gmail.com>
2025-08-24 19:34:54 +0300

    ilk commit

---
+++ src/components/Currency.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import '../css/currency.css';  //  Stil dosyamızı dahil ediyoruz
+import { MdArrowCircleRight } from "react-icons/md";
+
+function Currency() {
+  return (
+    <div className='currency-div'> 
+      
+      {/* Başlık */}
+      <h3 className='baslık'>DÖVİZ KURU UYGULAMASI</h3>
+      
+      {/*  Form alanı */}
+      <div className="currency-form">
+        
+        {/*  Soldaki input + select grubu */}
+        <div className="currency-group">
+          {/* Kullanıcı miktar giriyor */}
+          <input 
+            type="number" 
+            className='amount' 
+            placeholder="Miktar" 
+          />
+          
+          {/* Çevrilecek para birimi seçiliyor */}
+          <select className='from-currency-option'>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>TL</option>
+          </select>
+        </div>
+        
+        {/*  Ortadaki ok ikonu */}
+        <MdArrowCircleRight className="arrow-icon" />
+
+        {/*  Sağdaki select + result grubu */}
+        <div className="currency-group">
+          {/* Hedef para birimi seçiliyor */}
+          <select className='to-currency-option'>
+            <option>TL</option>
+            <option>EUR</option>
+            <option>USD</option>
+          </select>
+
+          {/* Çeviri sonucu burada gösterilecek */}
+          <input 
+            type="number" 
+            className='result' 
+            placeholder="Sonuç" 
+            readOnly 
+          />
+        </div>
+      </div>
+
+      {/* Çevir Butonu */}
+      <button className='button'>ÇEVİR</button>
+
+    </div>
+  )
+}
+
+export default Currency;
